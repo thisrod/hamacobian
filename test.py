0d49c1952147ce7449fc3e21f6ffd31e6ee55e2b
Rodney <thisrod@gmail.com>
2012-08-15 14:56:28 +1000

    First working version.

--- test.py
+++ test.py
@@ -11,67 +11,47 @@ class Failure(Exception):
 	Subclasses define an exclude method, which raises the exception if appropriate.  This might be a class method, if there's no need to construct an instance that won't be raised."""
 	
 	@classmethod
-	set_up(class, operation, z):
-		"""Assert that z is a valid ensemble, and prepare to test operation.
-		Return the number of components and modes+1.
-		The result is stored in the class variable Failure.result"""
-		
+	def set_up(cls, operation, z):
+		"Assert that z is a valid ensemble, and prepare to test operation."
 		assert isinstance(z, ndarray)
 		assert z.ndim == 2
 		n, m = z.shape
 		assert m == 2	# Single mode implementation restriction
-		class.operation = operation
-		class.ensemble = z.copy()
-		class.result = operation(z)
-		return n, m
+		cls.operation = operation
+		cls.input = z.copy()
+		cls.output = operation(z)
 		
-class FailureToRelate(Failure):
-	"""Test that some property of the result equals the value supplied on construction.
-	Subclasses should override the property() method, which defaults to the identity."""	
-	
-	def __init__(self, expected):
-		self.expected = expected
-	
-	def exclude(self):
-		self.actual = self.property(result)
-		self.relates(self.actual, self.expected) or raise self
-		
-	def property(self, result):
-		result
-		
-class FailureToEqual(FailureToRelate):
-	def relates(self, actual, expected):
-		actual == expected
-		
-class FailureToBe(FailureToRelate):
-	def relates(self, actual, expected):
-		actual is expected
-		
-
-class NotAMatrix(FailureToBe):
+class NotAMatrix(Failure):
 	"A function that should have returned an ndarray returned something else."
-	def __init__(self):
-		self.expected = ndarray
-	def property(self, result):
-		type(result)
+	@classmethod
+	def exclude(cls):
+		if not isinstance(cls.output, ndarray): raise cls
 
-class WrongShape(FailureToEqual):
+class WrongShape(Failure):
 	"The shape of a calculated ndarray was inconsistent with the shape of the input."
-	def __init__(self, *s):
-		self.expected = s
-	def property(self, result):
-		result.shape
+	@classmethod
+	def exclude(cls, *s):
+		if cls.output.shape != s: raise cls
 		
 class Nonhermitian(Failure):
 	"""The result is not Hermitian.  Actual is a set of indices of elements that aren't the conjugate of their opposites."""
-	pass
+	@classmethod
+	def exclude(cls):
+		pass
+	
+class NotCatenation(Failure):
+	@classmethod
+	def exclude(cls):
+		pass
 
 
 def test_rho(rho, jsq, jham, z):
-	n, m = Failure.set_up(rho, z)
+	Failure.set_up(rho, z)
+	n, m = z.shape
 	NotAMatrix.exclude()
 	Nonhermitian.exclude()
 	WrongShape.exclude(n,n)
+	NotCatenation.exclude()
 	
 	
 def test_jsq(rho, jsq, jham, z):
