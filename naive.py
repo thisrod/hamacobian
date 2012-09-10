5046cddbf4889408ccf0a040c9b1cb10e9dfaa44
Rodney Polkinghorne <thisrod@gmail.com>
2012-09-10 15:23:55 +1000

    Added Levenberg-Marquardt support

--- naive.py
+++ naive.py
@@ -10,9 +10,12 @@ def extract(z):
 	# Convention: f stores a column vector of phis, A stores a matrix whose rows are alpha vectors.
 	return z.shape + (z[:,0:1], z[:,1:])
 	
-def rho(z):
-	n, m, f, A = extract(matrix(z))
-	logrho = f.H + f + A*A.H
+def rho(z, w):
+	"""Calculate the matrix of inner products between the components of two states."""
+	nl, ml, fl, Al = extract(matrix(z))
+	nr, mr, fr, Ar = extract(matrix(w))
+	assert nl == nr and ml == mr
+	logrho = fl.H + fr + Ar*Al.H
 	return exp(logrho)
 	
 def jsq(z, rho):
@@ -25,3 +28,21 @@ def jsq(z, rho):
 	for q in xrange(1,m):
 		poly[q:n:n*m,q:n:n*m] += 1
 	return array(poly)*array(rho).repeat(m,0).repeat(m,1)
+
+def amb(z, h, rho):
+	"""Calculate the A-B terms in the Levenberg-Marquardt H, between amplitudes z and z+h.
+	rho gives the inner products between states <z+h| and |z>"""
+	n, m, f, A = extract(matrix(z))
+	n, m, df, dA = extract(matrix(h))
+	expt = array(df+dA*(A+dA).H)
+	w = array(dz)
+	w[:,0] = 0
+	w = w.reshape(-1,1)
+	v = array(z)
+	v[:,0] = 1
+	v = w.reshape(-1,1)
+	return array(rho).repeat(m,0)*(w*exp(expt).repeat(m,0) + v*expm1(expt).repeat(m,0))
+	
+def jham(z, h, rho):
+	"""Calculate the Hamiltonian bracket in the Levenberg-Marquardt H, between amplitudes z and z+h.
+	rho gives the inner products between states <z+h| and |z>"""
