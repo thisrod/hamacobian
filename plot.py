08dad361e07c8c04ea00abd757cc752f7fd863e6
Rodney Polkinghorne <thisrod@gmail.com>
2012-09-10 12:24:57 +1000

    Plotting an ensemble draws the markers

--- plot.py
+++ plot.py
@@ -1,22 +1,37 @@
 # See README.md
+#
+# In principle, this should be a front end for sciplot.  That involves too much red tape for the moment, so I'm using Metapost.
+
 
 from tempfile import mkdtemp
 from os.path import join
 from subprocess import check_call
+from cmath import pi, phase, exp
 
-mpscript = """input plot;
+mpprefix = """input plot;
 
 beginfig(1)
-	draw marker scaled 1in;
-endfig;
+"""
+
+mpsuffix = """endfig;
 end
 """
 
-def plotens():
+def plot_cpt(wgt, amp):
+	"Plot a marker for a coherent state with the given coherent amplitude and logarithmic weight-phase."
+	
+	mpfd.write("mark(%f, %f, %f, %f);\n" %
+		(amp.real, amp.imag, abs(exp(wgt)), 180/pi*phase(exp(wgt))))
+
+def plotens(z):
+	global mpfd
 	amp = join(mkdtemp(), "a.mp")
 	mpfd = open(amp, "w")
 	print mpfd
-	mpfd.write(mpscript)
+	mpfd.write(mpprefix)
+	for r in z:
+		plot_cpt(r[0], r[1])
+	mpfd.write(mpsuffix)
 	mpfd.close()
 	check_call(["mpost", amp])
 	check_call(["xetex", "mpsproof", "a.1"])
