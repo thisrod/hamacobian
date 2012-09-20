8cdfb3a810e87dcdb1d9fe450cacbbd70af5a78b
Rodney Polkinghorne <thisrod@gmail.com>
2012-09-20 11:01:23 +1000

    focancel varies the amplitude of a cat state, and tests how well it fits the halfway state.

--- focancel.py
+++ focancel.py
@@ -8,18 +8,18 @@ from scipy.misc import factorial
 	
 alpha = 2+0j
 n = array(xrange(20))
-# coefficients of the halfway state expanded over Fock states
-# the truncation error has norm 1.4e-4 
+# the truncation error has norm 1e-4 
 original = exp(-0.5*abs(alpha)**2) * alpha**n / sqrt(factorial(n))
+original /= sqrt(sum(original**2))
+# coefficients of the halfway state expanded over Fock states
 halfway = (-1)**(n*(n-1)/2) * original
-halfway /= sqrt(sum(halfway**2))
 
 def lnormsq(z):
 	return log(sum(rho(z,z))).real
 	
 def residual(z):
 	"squared Hilbert space distance between the ensemble z and the original state"
-	bkts = jfock(z, original)
+	bkts = jfock(z, halfway)
 	return 1 + exp(lnormsq(z)) - 2*sum(bkts[::2]).real
 
 def tikstep(A, y, epsilon):
@@ -27,13 +27,19 @@ def tikstep(A, y, epsilon):
 	A = A.copy() + epsilon*identity(y.size)
 	return gesv(A, y)
 	
-def lmstep():
-	pass
+def lmstep(z, epsilon):
+	"update the guess or the confidence interval.  in this version, we keep epsilon fixed and return the linear guess."
+	r = rho(z,z)
+	V = jsq(z, rho)
+	rhs = jlft(z, rho) - jfock(z, halfway)
+	return tikstep(V, rhs, epsilon), epsilon
+		
 	
 
 for x in linspace(1.8, 2.2, 11):
-	zs = array([[0, x]])
+	zs = array([[0, x*1j], [0, -x*1j]])
+	# zs = array([[0, x]])
 	zs[:,0] -= 0.5*lnormsq(zs)
 	print "%.2f    %.5f" % (x, sqrt(residual(zs)))
 
-# plotens(z)
+# plotens(zs)
