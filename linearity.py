01815cdee1c12d115f3b47dbaffae8a69a32bf7d
Rodney Polkinghorne <thisrod@gmail.com>
2012-10-18 17:20:30 +1100

    added numerical stability graph to linearity test

--- linearity.py
+++ linearity.py
@@ -18,6 +18,11 @@ q = Ket(Sum(*(DisplacedState(randc(), v) for i in xrange(R))))
 m = []	# slopes
 rpts = 7
 
+pylab.figure("Curvature")
+pylab.title('%d coherent states linearly approximated in random directions' % R)
+pylab.xlabel('norm of parameter shift')
+pylab.ylabel('relative error in linear approximation')
+
 for p in xrange(rpts):
 	# choose a random direction
 	drn = col(randc() for i in q.prms().indices())
@@ -39,10 +44,44 @@ for p in xrange(rpts):
 	pylab.loglog(x, [rslt[i] for i in x])
 	pylab.hold(True)
 	
-pylab.title('%d coherent states linearly approximated in random directions' % R)
-pylab.xlabel('norm of parameter shift')
-pylab.ylabel('relative error in linear approximation')
 pylab.text(0.001, 0.01, "slope %.3f pm %.3f" % 
 	(sum(m).real/len(m), sqrt(sum(x**2 for x in m)/rpts - sum(m)**2/rpts**2).real))
+
+pylab.figure("Expansions")
+
+dst = [abs(exp(x)) for x in pylab.linspace(log(1e-10), log(1e-2), 50)]
 	
+p = 7		# maximum particle number
+r1 = [ [] for i in xrange(p) ]
+r2 = [ [] for i in xrange(p) ]
+r3 = [ [] for i in xrange(p) ]
+
+for h in dst:
+	shft = q.smrp(q.prms()+h*drn) - q
+	lin = q.D()*h*drn
+	for n in xrange(p):
+		r1[n].append(abs(Bra(number(n))*shft))
+		r2[n].append(abs(Bra(number(n))*lin))
+		r3[n].append(abs(Bra(number(n))*(shft - lin)))
+
+pylab.subplot(311)		
+pylab.hold(True)
+for i in xrange(p):
+	pylab.loglog(dst, r1[i])
+pylab.title('finite difference')
+
+pylab.subplot(312)		
+pylab.hold(True)
+for i in xrange(p):
+	pylab.loglog(dst, r2[i])
+pylab.ylabel(r'weight of $|n\rangle$')
+pylab.title('scaled derivative')
+
+pylab.subplot(313)		
+pylab.hold(True)
+for i in xrange(p):
+	pylab.loglog(dst, r3[i])
+pylab.title('discrepency')
+	
+pylab.xlabel('norm of parameter shift')
 pylab.show()
