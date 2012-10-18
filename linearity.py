6d2193fc9b3ebf3563faf0f6669e503133a70b47
Rodney Polkinghorne <thisrod@gmail.com>
2012-10-18 17:38:27 +1100

    numerical instability graph improved

--- linearity.py
+++ linearity.py
@@ -9,7 +9,8 @@ def randc():
 	return gauss(0,1) + gauss(0,1)*1j
 
 R = int(argv[1])		# number of states in ensemble
-dst = [abs(exp(x)) for x in pylab.linspace(log(1e-4), log(5), 50)]
+dst = [abs(exp(x)) for x in pylab.linspace(log(1e-5), log(5), 50)]
+basis = col(Bra(number(n)) for n in xrange(20))
 
 # choose a random ensemble
 v = FockExpansion(1)
@@ -18,7 +19,7 @@ q = Ket(Sum(*(DisplacedState(randc(), v) for i in xrange(R))))
 m = []	# slopes
 rpts = 7
 
-pylab.figure("Curvature")
+pylab.subplot(211)
 pylab.title('%d coherent states linearly approximated in random directions' % R)
 pylab.xlabel('norm of parameter shift')
 pylab.ylabel('relative error in linear approximation')
@@ -29,59 +30,26 @@ for p in xrange(rpts):
 	drn /= norm(drn)
 	
 	# plot how the approximation fails with distance
-	rslt = {}
+	sqt = []
+	bss = []
 	for h in dst:
 		shft = q.smrp(q.prms()+h*drn) - q
 		lin = q.D()*h*drn
-		rslt[h] = norm(shft - lin)/norm(lin)
+		sqt.append( norm(shft - lin)/norm(lin) )
+		bss.append( norm(basis*(shft - lin))/norm(basis*lin) )
 
 	# slope in the quadratic region between 1e-3 and 1e-2
-	m.append( (log(rslt[dst[21]]) - log(rslt[dst[10]])) / \
-		(log(dst[21]) - log(dst[10])) )
-		
-	x = rslt.keys()
-	x.sort()
-	pylab.loglog(x, [rslt[i] for i in x])
+#	m.append( (log(sqt[21]) - log(sqt[10])) / \
+#		(log(dst[21]) - log(dst[10])) )
+
+	pylab.subplot(211)
+	pylab.loglog(dst, sqt)
+	pylab.hold(True)
+	pylab.subplot(212)
+	pylab.loglog(dst, bss)
 	pylab.hold(True)
 	
-pylab.text(0.001, 0.01, "slope %.3f pm %.3f" % 
-	(sum(m).real/len(m), sqrt(sum(x**2 for x in m)/rpts - sum(m)**2/rpts**2).real))
+#pylab.text(0.001, 0.01, "slope %.3f pm %.3f" % 
+#	(sum(m).real/len(m), sqrt(sum(x**2 for x in m)/rpts - sum(m)**2/rpts**2).real))
 
-pylab.figure("Expansions")
-
-dst = [abs(exp(x)) for x in pylab.linspace(log(1e-10), log(1e-2), 50)]
-	
-p = 7		# maximum particle number
-r1 = [ [] for i in xrange(p) ]
-r2 = [ [] for i in xrange(p) ]
-r3 = [ [] for i in xrange(p) ]
-
-for h in dst:
-	shft = q.smrp(q.prms()+h*drn) - q
-	lin = q.D()*h*drn
-	for n in xrange(p):
-		r1[n].append(abs(Bra(number(n))*shft))
-		r2[n].append(abs(Bra(number(n))*lin))
-		r3[n].append(abs(Bra(number(n))*(shft - lin)))
-
-pylab.subplot(311)		
-pylab.hold(True)
-for i in xrange(p):
-	pylab.loglog(dst, r1[i])
-pylab.title('finite difference')
-
-pylab.subplot(312)		
-pylab.hold(True)
-for i in xrange(p):
-	pylab.loglog(dst, r2[i])
-pylab.ylabel(r'weight of $|n\rangle$')
-pylab.title('scaled derivative')
-
-pylab.subplot(313)		
-pylab.hold(True)
-for i in xrange(p):
-	pylab.loglog(dst, r3[i])
-pylab.title('discrepency')
-	
-pylab.xlabel('norm of parameter shift')
 pylab.show()
