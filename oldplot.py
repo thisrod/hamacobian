89216437a760bf8840e2f1f58597edc2220bdb5d
Rodney Polkinghorne <thisrod@gmail.com>
2012-11-20 10:24:34 +1100

    First version of stiffness experiment.  Spurious results due to Sum re-ordering terms.

---
+++ oldplot.py
@@ -0,0 +1,79 @@
+# See README.md
+#
+# In principle, this should be a front end for sciplot.  That involves too much red tape for the moment, so I'm using Metapost.
+#
+# The Metapost variable u is the scale for coherent amplitudes, and v is the scale for weight circles.
+
+
+from tempfile import mkdtemp
+from os import chdir
+from os.path import dirname, join
+from subprocess import check_call
+from math import pi, exp
+import numpy as np
+
+
+def plot_cpt(wgt, amp):
+	"Plot a marker for a coherent state with the given coherent amplitude and logarithmic weight-phase."
+	
+	mpfd.write("mark(%f, %f, %f, %f);\n" %
+		(amp.real, amp.imag, exp(wgt.real+0.5*abs(amp)**2), 180/pi*wgt.imag))
+
+
+def plotens(z):
+	chdir(mkdtemp())
+	global mpfd
+	mpfd = open("a.mp", "w")
+	plot_begin();
+	plot_scale(z);
+	mpfd.write("\nbeginfig(1)\n")
+	for r in z:
+		plot_cpt(r[0], r[1])
+	mpfd.write("path p; p := bbox currentpicture;\ndraw fullcircle scaled 2u withcolor junk;\nclip currentpicture to p;\n")
+	mpfd.write("endfig;\nend\n")
+	mpfd.close()
+	check_call(["mpost", "a.mp"])
+	check_call(["xetex", "mpsproof", "a.1"])
+	check_call(["open", "mpsproof.pdf"])
+
+
+def plot_scale(z):
+	"""generate Metapost code to set the scales u and v.  u, the coherent amplitude scale, is set so the markers fit in a 2in square.  v, the weight scale, is set so the maximum radius is 1/3 the median distance to the nearest mark."""
+	if z.shape[0] == 1:
+		a = max(2, abs(z[0,1])+1)
+	else:
+		a = max(z[:,1].real.max() - z[:,1].real.min(), z[:,1].imag.max() - z[:,1].imag.min())
+	mpfd.write("u*%f = 1in;\n" % a)
+	
+	amps = z[:,1:2]
+	w = np.repeat(amps.T, amps.size, 0)
+	for i in range(amps.size):
+		w[i,i] = np.inf
+	b = np.median(abs(w-amps).min(0))
+	c = np.exp(z[:,0].real+0.5*np.abs(z[:,1])**2).max()
+	mpfd.write("%f*v = %f*u*2/3;\n" % (c, b))
+	
+
+
+def plot_begin():
+	"""Generate Metapost definitions"""
+
+	mpfd.write("""path marker;
+marker = subpath ((3.2/7, 5.37/7)*(length fullcircle)) of fullcircle
+	-- (0,0)
+	-- subpath ((5.63/7, 7.8/7)*(length fullcircle)) of fullcircle
+	-- point (7.8/7*length fullcircle) of fullcircle
+	-- 1.1*(point (9/7 length fullcircle) of fullcircle)
+	--point (10.2/7*length fullcircle) of fullcircle;
+
+color junk;
+junk = 0.7 white;
+
+def mark(expr ax, ay, w, f) =
+	draw marker scaled (w*v) rotated f shifted (u*(ax, ay));
+enddef;\n\n""")
+
+if __name__=="__main__":
+	"Test data"
+	z = np.array([[0,0.8],[2,1+0.2j],[0+1j,1.2]])
+	plotens(z)
---
+++ regular.py
@@ -0,0 +1,92 @@
+"""Investigate the performance of different regularisation methods for the quartic oscillator"""
+
+from random import gauss
+from states import *
+import pylab
+from plot import plotens
+
+def randc(): return gauss(0,1) + gauss(0,1)*1j
+
+dmax = 2e-10		# range to shift z
+R = 3			# size of ensemble
+gs = 10		# points to plot
+rpts = 10		# directions to try
+p = 20		# basis size
+
+ham = lop.conjugate()*lop.conjugate()*lop*lop
+
+def zdot(ql, qr):
+	"return zdot such that |ql>zdot = -iH|qr>"
+	lhs = basis*ql.D()
+	iHq = (-1j)*(ham*qr)
+	rhs = basis*iHq
+	zd, r, rk, sing = numpy.linalg.lstsq(lhs, rhs)
+	zd = col(zd.flatten())
+	return zd
+	
+# choose a random ensemble about amplitude 2
+basis = col(Bra(number(n)) for n in xrange(p))
+v = FockExpansion(1)
+q = Ket(Sum(*(DisplacedState(2+randc(), v) for i in xrange(R))))
+q /= norm(q)
+x = [float(x) for x in pylab.linspace(0,dmax, gs)]
+
+zd = zdot(q,q)
+foo, sing, bar = numpy.linalg.svd(basis*q.D())
+	
+pylab.subplot(321)
+pylab.title("changing projection")
+pylab.subplot(323)
+pylab.title("changing state")
+pylab.ylabel("$|\dot{z} - \dot{z}_0|$", size=16)
+pylab.subplot(325)
+pylab.title("changing both")
+pylab.xlabel("norm of change in z")
+pylab.subplot(324)
+pylab.title("2 norm of Jacobian change")
+
+pylab.subplot(343)
+pylab.title("ensemble relative to unit circle")
+plotens(q)
+
+pylab.subplot(344)
+pylab.title("singular values of Jacobian")
+pylab.semilogy(sing, 'o', mfc='none')
+pylab.xlim([-0.5, sing.size-0.5])
+
+pylab.gcf().text(0.55,0.2, "relative residual: %.2f" % (norm(q.D()*zd - (-1j)*ham*q)/norm(ham*q)))
+pylab.gcf().text(0.55,0.3, "norm zdot: %.1f" % norm(zd))
+
+pylab.gcf().text(0.5,0.95, "Change of zdot for quartic oscillator, as z changes in random directions", ha='center')
+
+for k in xrange(rpts):
+	left = []
+	right = []
+	both = []
+	cnum = []
+	
+	# choose a random direction
+	drn = col(randc() for i in q.prms().indices())
+	drn /= norm(drn)
+	
+	for eps in x:
+		q1 = q.smrp(q.prms()+eps*drn)
+		left.append(norm(zd-zdot(q1,q)))
+		right.append(norm(zd-zdot(q,q1)))
+		both.append(norm(zd-zdot(q1,q1)))
+		cnum.append(numpy.linalg.norm(basis*(q1.D()-q.D()), 2))
+	
+	pylab.subplot(321)
+	pylab.semilogy(x, left, hold=True)
+	pylab.subplot(323)
+	pylab.semilogy(x, right, hold=True)
+	pylab.subplot(325)
+	pylab.semilogy(x, both, hold=True)
+	pylab.subplot(324)
+	pylab.semilogy(x, cnum, hold=True)
+	
+pylab.show()
+
+
+# print "%.3f, %3.0f, %3.0f, %3.0f" % (rerr, norm(zdot), norm(iHq), 
+# 	sum(norm(Ket(t)) for t in iHq.s.terms()))
